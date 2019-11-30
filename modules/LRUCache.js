16ad33d41543775d4a22888ce2abde2136eb79eb
Alexey Raspopov <alexey.raspopov@datarobot.com>
2019-11-29 19:41:08 -0500

    replace LRUCache with a better time/space-wise implementation
    With fixed size doubly-linked lists as described in https://yomguithereal.github.io/posts/lru-cache

--- modules/LRUCache.js
+++ modules/LRUCache.js
@@ -1,34 +1,126 @@
 export default function LRUCache(capacity, onDelete) {
-  let map = new Map();
+  let forward = createPointerArray(capacity);
+  let backward = createPointerArray(capacity);
+  let keys = new Array(capacity);
+  let values = new Array(capacity);
+  let cursor = 0;
+  let head = 0;
+  let tail = 0;
+  let items = new Map();
+
+  function liftUp(pointer) {
+    if (head === pointer) {
+      return;
+    }
+
+    let prev = backward[pointer];
+    let next = forward[pointer];
+
+    if (tail === pointer) {
+      tail = prev;
+    } else {
+      backward[next] = prev;
+    }
+
+    forward[prev] = next;
+    backward[head] = pointer;
+    forward[pointer] = head;
+    head = pointer;
+  }
+
+  function siftDown(pointer) {
+    if (tail === pointer) {
+      return;
+    }
+
+    let next = forward[pointer];
+    let prev = backward[pointer];
+
+    if (head === pointer) {
+      head = next;
+    } else {
+      forward[prev] = next;
+    }
+
+    backward[next] = prev;
+    forward[tail] = pointer;
+    backward[pointer] = tail;
+    tail = pointer;
+  }
+
   return {
     has(key) {
-      return map.has(key);
+      return items.has(key);
     },
     get(key) {
-      let value = map.get(key);
-      if (value != null) {
-        map.delete(key);
-        map.set(key, value);
+      let pointer = items.get(key);
+
+      if (typeof pointer !== 'undefined') {
+        liftUp(pointer);
+        return values[pointer];
       }
-      return value;
     },
     set(key, value) {
-      map.delete(key);
-      map.set(key, value);
-      if (map.size > capacity) {
-        let firstEntry = map.entries().next();
-        if (!firstEntry.done) {
-          let entry = firstEntry.value;
-          map.delete(entry[0]);
-          onDelete(entry[1]);
-        }
+      let pointer = items.get(key);
+
+      if (typeof pointer !== 'undefined') {
+        liftUp(pointer);
+        values[pointer] = value;
+        return;
       }
+
+      if (cursor < capacity) {
+        pointer = cursor++;
+      } else {
+        pointer = tail;
+        tail = backward[pointer];
+        items.delete(keys[pointer]);
+        onDelete(values[pointer]);
+      }
+
+      items.set(key, pointer);
+      keys[pointer] = key;
+      values[pointer] = value;
+
+      forward[pointer] = head;
+      backward[head] = pointer;
+      head = pointer;
     },
     delete(key) {
-      map.delete(key);
+      let pointer = items.get(key);
+
+      if (typeof pointer === 'undefined') {
+        return;
+      }
+
+      siftDown(pointer);
+      items.delete(key);
+      onDelete(values[pointer]);
     },
     size() {
-      return map.size;
+      return items.size;
     },
   };
+}
+
+const MAX_8BIT_INTEGER = Math.pow(2, 8) - 1;
+const MAX_16BIT_INTEGER = Math.pow(2, 16) - 1;
+const MAX_32BIT_INTEGER = Math.pow(2, 32) - 1;
+
+function createPointerArray(size) {
+  let maxIndex = size - 1;
+
+  if (maxIndex <= MAX_8BIT_INTEGER) {
+    return new Uint8Array(size);
+  }
+
+  if (maxIndex <= MAX_16BIT_INTEGER) {
+    return new Uint16Array(size);
+  }
+
+  if (maxIndex <= MAX_32BIT_INTEGER) {
+    return new Uint32Array(size);
+  }
+
+  return new Float64Array(size);
 }
