6d4b98b916e1ac084f7f0547a5c978224262d7df
Alexey Raspopov <oleksii.raspopov@gmail.com>
2020-01-27 00:09:49 -0500

    create flowtype and typescript lib definitions

---
+++ typings/ReactWarehouse.d.ts
@@ -0,0 +1,41 @@
+export as namespace ReactWarehouse;
+
+type ResourceCache<T, D> = Object;
+
+type Resource<T> = Object;
+
+type PendingQueryResult<T> = T | Promise<T> | [Promise<T>, () => void];
+
+type QueryFunction<T, D> = (...deps: D) => PendingQueryResult<T>;
+
+type ResourceOptions<T, D> = {
+  query: QueryFunction<T, D>;
+  maxAge?: number;
+  capacity?: number;
+};
+
+export function createResource<T, D>(
+  options: ResourceOptions<T, D>,
+): ResourceCache<T, D>;
+
+export function useResource<T, D>(
+  Resource: ResourceCache<T, D>,
+  deps: D,
+): Resource<T>;
+
+export function useResourceFactory<T, D>(
+  query: QueryFunction<T, D>,
+  deps: D,
+): Resource<T>;
+
+export function useResourceFlow<T, D>(
+  Resource: ResourceCache<T, D>,
+  deps: D,
+): [Resource<T>, boolean];
+
+export function useResourceSync<T, D>(
+  Resource: ResourceCache<T, D>,
+  deps: D,
+): T;
+
+export function useResourceValue<T>(resource: Resource<T>): T;
