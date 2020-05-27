c2b0b64e5a32678691ff23f40ef0fa179368a2a9
Alexey Raspopov <alexey.raspopov@datarobot.com>
2020-05-27 19:27:45 -0400

    use esm bundle only

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import babel from 'rollup-plugin-babel';
 import copy from 'rollup-plugin-copy';
 import bundleSize from 'rollup-plugin-bundle-size';
 import multiEntry from 'rollup-plugin-multi-entry';
@@ -6,15 +5,8 @@ import autoExternal from 'rollup-plugin-auto-external';
 
 export default {
   input: 'modules/*.js',
-  output: [
-    { file: 'build/bundle.js', format: 'cjs' },
-    { file: 'build/module.js', format: 'esm' },
-  ],
+  output: [{ file: 'build/module.js', format: 'esm' }],
   plugins: [
-    babel({
-      babelrc: false,
-      presets: [['@babel/preset-env', { loose: true }], '@babel/preset-react'],
-    }),
     copy({
       targets: [{ src: 'typings/*', dest: 'build' }],
     }),
