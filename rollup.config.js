3ac5860c85c13d77197d57858f15a1bef35d8cc2
Alexey Raspopov <alexey.raspopov@datarobot.com>
2021-01-18 18:56:50 -0500

    revert babel and module changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import babel from 'rollup-plugin-babel';
 import copy from 'rollup-plugin-copy';
 import bundleSize from 'rollup-plugin-bundle-size';
 import multiEntry from 'rollup-plugin-multi-entry';
@@ -5,8 +6,15 @@ import autoExternal from 'rollup-plugin-auto-external';
 
 export default {
   input: 'modules/*.js',
-  output: [{ file: 'build/module.js', format: 'esm' }],
+  output: [
+    { file: 'build/bundle.js', format: 'cjs' },
+    { file: 'build/module.js', format: 'esm' },
+  ],
   plugins: [
+    babel({
+      babelrc: false,
+      presets: [['@babel/preset-env', { loose: true }], '@babel/preset-react'],
+    }),
     copy({
       targets: [{ src: 'typings/*', dest: 'build' }],
     }),
