bb42b090cf80a75c99904a655ebb3075dc660cdd
Alexey Raspopov <oleksii.raspopov@gmail.com>
2021-04-13 11:52:44 -0400

    revert experimental typings update

--- typings/bundle.d.ts
+++ typings/bundle.d.ts
@@ -3,7 +3,7 @@ import { Node, Component } from 'react';
 
 export type ResourceCache<Data, Deps, Vars> = {};
 
-export type Resource<Data, Vars = Data> = {};
+export type Resource<Data> = {};
 
 export type ResourceQuery<Data> = Promise<Data> | [Promise<Data>, () => void];
 
@@ -31,30 +31,30 @@ export function createResource<Data, Deps, Vars>(
   options: ResourceOptions<Data, Deps, Vars>,
 ): ResourceCache<Data, Deps, Vars>;
 
-export function useResource<Data, Deps, Vars>(
-  Resource: ResourceCache<Data, Deps, Vars>,
+export function useResource<Data, Deps>(
+  Resource: ResourceCache<Data, Deps, *>,
   deps: Deps,
-): Resource<Data, Vars>;
+): Resource<Data>;
 
-export function experimental_useResource<Data, Deps, Vars>(
-  Resource: ResourceCache<Data, Deps, Vars>,
+export function experimental_useResource<Data, Deps>(
+  Resource: ResourceCache<Data, Deps, *>,
   deps: Deps,
-): Resource<Data, Vars>;
+): Resource<Data>;
 
-export function useResourceFactory<Data, Deps, Vars>(
+export function useResourceFactory<Data, Deps>(
   query: QueryFunction<Data, Deps>,
   deps: Deps,
-): Resource<Data, Vars>;
+): Resource<Data>;
 
-export function experimental_useResourceFactory<Data, Deps, Vars>(
+export function experimental_useResourceFactory<Data, Deps>(
   query: QueryFunction<Data, Deps>,
   deps: Deps,
-): Resource<Data, Vars>;
+): Resource<Data>;
 
-export function useResourceFlow<Data, Deps, Vars>(
-  Resource: ResourceCache<Data, Deps, Vars>,
+export function useResourceFlow<Data, Deps>(
+  Resource: ResourceCache<Data, Deps, *>,
   deps: Deps,
-): [Resource<Data, Vars>, boolean];
+): [Resource<Data>, boolean];
 
 export function useResourceSync<Data, Deps>(
   Resource: ResourceCache<Data, Deps, *>,
@@ -68,22 +68,20 @@ export function experimental_useResourceSync<Data, Deps>(
 
 export function useResourceMutation<Data, Vars>(
   Resource: ResourceCache<Data, *, Vars>,
-  resource: Resource<Data, Vars>,
+  resource: Resource<Data>,
 ): MutateFunction<Data, Vars>;
 
-export function useResourceValue<Data>(resource: Resource<Data, *>): Data;
+export function useResourceValue<Data>(resource: Resource<Data>): Data;
 
-export function experimental_useResourceValue<Data>(resource: Resource<Data, *>): Data;
+export function experimental_useResourceValue<Data>(resource: Resource<Data>): Data;
 
-export function experimental_useResourceState<Data, Vars>(
-  resource: Resource<Data, Vars>,
-): [Data, (Vars) => void];
+export function experimental_useResourceState<Data>(
+  resource: Resource<Data>,
+): [Data, (Data) => void];
 
-export function experimental_useResourcePendingState<Data>(resource: Resource<Data, *>): boolean;
+export function experimental_useResourcePendingState<Data>(resource: Resource<Data>): boolean;
 
-export function experimental_useResourceRetryCallback<Data>(
-  resource: Resource<Data, *>,
-): () => void;
+export function experimental_useResourceRetryCallback<Data>(resource: Resource<Data>): () => void;
 
 type ErrorBoundaryProps = {
   fallback: Node;
